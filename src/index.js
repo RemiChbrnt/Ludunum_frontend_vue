5368c50b43cc850e6a4c9174c28166fb6873c9e1
unknown <remi.chabrant@gmail.com>
2021-05-12 08:22:56 +0200

    connection to backend and first graphql Query

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import Vue from 'vue'
+import Vue from "vue"
 import App from './components/App.vue'
 
-Vue.config.productionTip = false
+import ApolloClient from 'apollo-boost'
+import VueApollo from "vue-apollo";
+
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    uri: 'http://localhost:3000/graphql'
+  })
+});
+
+Vue.use(VueApollo); // use middleware
 
 new Vue({
   el: '#app',
+  apolloProvider, // add option
   render: h => h(App)
 })
